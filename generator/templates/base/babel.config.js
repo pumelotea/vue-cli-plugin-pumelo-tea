d48863da270b02f40fd6d2c6f3b0a7e0ba4f351f
pumelotea <535553297@qq.com>
2019-10-10 08:59:05 +0800

    生产环境自动移除console

--- generator/templates/base/babel.config.js
+++ generator/templates/base/babel.config.js
@@ -1,14 +1,34 @@
+const plugins = ["@vue/babel-plugin-transform-vue-jsx"]
+// 生产环境移除console
+if(process.env.NODE_ENV === 'production') {
+  plugins.push("transform-remove-console")
+}
 module.exports = {
+  plugins: plugins,
   presets: [
     [
-      '@vue/app',
-      {
-        polyfills: [
-          'es6.promise',
-          'es6.symbol'
-        ],
-        useBuiltIns: 'entry'
-      }
+      '@vue/app', {
+      modules: false,
+      polyfills: [
+        'es6.promise',
+        'es6.symbol'
+      ],
+      useBuiltIns: 'entry',
+    }
     ]
   ]
 }
+// module.exports = {
+//   presets: [
+//     [
+//       '@vue/app',
+//       {
+//         polyfills: [
+//           'es6.promise',
+//           'es6.symbol'
+//         ],
+//         useBuiltIns: 'entry'
+//       }
+//     ]
+//   ]
+// }
