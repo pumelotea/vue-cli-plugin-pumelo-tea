f330ef2c605a2d54b585bc930fecc6682f9a07db
pumelotea <535553297@qq.com>
2019-12-04 15:14:24 +0800

    增加docker支持

--- generator/templates/base/docker-scripts.sh
+++ generator/templates/base/docker-scripts.sh
@@ -1,6 +1,9 @@
 docker build . -t my-app
 
+#不带权限
 docker run -d -p 8080:8080 -it -v /Users/pumelotea/test:/test my-app
+#带权限
+docker run -d -p 5555:8080 --privileged=true -it -v /var/docker_logs:/test docker.ltit.info:5000/my-app:v1
 
 docker exec -it 6717dc943b7a7f9c6d1aa694f08f2eb990aec2e4aab33769c0209a717f83a48b /bin/bash
 
@@ -9,3 +12,8 @@ docker save > my-app.img my-app:latest
 docker load < my-app.img
 
 docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+
+docker rm $(sudo docker ps -a -q)
+
+#私有仓库
+docker run -d -p 5000:5000 --privileged=true -v /opt/registry:/tmp/registry registry
